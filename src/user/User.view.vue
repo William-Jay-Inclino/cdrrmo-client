1e0c587f1b46c643060ad16f25b0937ec266215b
William Jay Inclino <wjay.inclino@gmail.com>
2023-12-02 09:58:00 +0800

    fix action btns in table users

--- src/user/User.view.vue
+++ src/user/User.view.vue
@@ -73,14 +73,17 @@
                                                         {{ user.statusObj?.text }} 
                                                     </span> 
                                                 </td>
-                                                <td class="text-center">
-                                                    <button @click="onClickUpdate(user)" class="btn btn-light btn-sm">
-                                                        <i class="fas fa-fw fa-pencil-alt"></i>
-                                                    </button>
-                                                    <button @click="onClickResetPw(user)" class="btn btn-light btn-sm">
-                                                        <i class="fas fa-fw fa-lock text-warning"></i>
-                                                    </button>
-                                                </td>
+                                                <td class="d-flex justify-content-center">
+  <button @click="onClickUpdate(user)" class="btn btn-light btn-sm">
+    <i class="fas fa-fw fa-pencil-alt"></i>
+  </button>
+  <button @click="onClickResetPw(user)" class="btn btn-light btn-sm">
+    <i class="fas fa-fw fa-lock text-warning"></i>
+  </button>
+</td>
+
+
+
                                             </tr>
                                         </tbody>
                                     </table>
