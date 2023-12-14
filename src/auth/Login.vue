0b09115d10afe8a84acca583021d7fd5db9f4c27
William Jay Inclino <wjay.inclino@gmail.com>
2023-12-14 08:19:35 +0800

    update login bg and breadcrumbs update

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -10,7 +10,7 @@
 
   
       <!-- Main content with background image and overlay -->
-      <div class="main-content" :style="{ 'background-image': 'url(' + config.baseUrl + 'images/cdrrmo-v2/login-bg.jpg)' }">
+      <div :style="backgroundStyle" class="main-content">
         <div class="row mt-5 pt-5">
           <div class="col-lg-1 col-md-1"></div>
           <div class="col-lg-4 col-md-4 col-sm-10">
@@ -33,7 +33,7 @@
                   </div>
   
                   <!-- Submit button -->
-                  <button type="submit" class="btn btn-primary w-25 float-right">Login</button>
+                  <button type="submit" class="btn btn-primary float-right">Login</button>
                 </form>
               </div>
             </div>
@@ -47,7 +47,7 @@
   </template>
   
   <script setup lang="ts">
-    import { ref } from 'vue';
+    import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
     import { authService, authStore } from '.'; 
     import { routeNames } from '../common/constants'
     import { useRouter } from 'vue-router';
@@ -57,8 +57,21 @@
     const router = useRouter()
     const $auth = authStore()
 
+    const isMobile = ref(window.innerWidth <= 768);
     const username = ref('');
     const password = ref('');
+
+    onMounted(() => {
+      // Check the screen size on component mount
+      checkMobile();
+      // Listen for window resize events to update the isMobile flag
+      window.addEventListener('resize', checkMobile);
+    });
+
+    onBeforeUnmount(() => {
+      // Remove the resize event listener to avoid memory leaks
+      window.removeEventListener('resize', checkMobile);
+    });
   
     const login = async() => {
       const userloggedIn = await $auth.login({username: username.value, password: password.value})
@@ -100,6 +113,18 @@
       }
     };
 
+    const checkMobile = () => {
+      isMobile.value = window.innerWidth <= 768; // Adjust the breakpoint as needed
+    };
+
+    const backgroundStyle = computed(() => {
+      return {
+        'background-image': isMobile.value
+          ? 'none'
+          : `url(${config.baseUrl}images/cdrrmo-v3/login-bg.jpg)`,
+      };
+    });
+
 
   </script>
   
@@ -132,6 +157,12 @@
     background-position: center;
     min-height: 100vh; /* Ensure the background covers the entire viewport height */
     overflow: hidden; /* Hide overflow content */
+  }
+
+
+  /* Define a class to hide the background image on mobile */
+  .mobile-background {
+    background-image: none;
   }
   
   </style>
--- src/common/components/Breadcrumbs.vue
+++ src/common/components/Breadcrumbs.vue
@@ -1,14 +1,19 @@
 <template>
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li v-for="item in items" class="breadcrumb-item" aria-current="page">
-                <router-link :to="{name: item.route}" class="item" :class="{'text-secondary active-item': item.isActive}">
-                    <span>{{ item.text }}</span>
-                </router-link>
-            </li>
-        </ol>
-    </nav>
+    <div class="card mb-5">
+        <div class="card-body">
+            <nav aria-label="breadcrumb" class="m-0 p-0">
+                <ol class="breadcrumb mb-0">
+                    <li v-for="item in items" class="breadcrumb-item" aria-current="page">
+                        <router-link :to="{name: item.route}" class="item" :class="{'text-secondary active-item': item.isActive}">
+                            <span>{{ item.text }}</span>
+                        </router-link>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
 
 </template>
 
@@ -31,5 +36,6 @@
     .item{
         text-decoration: none; 
     }
+    
 
 </style>
