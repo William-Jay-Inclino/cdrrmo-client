c3cf75f6467636e7e6786fda0129d3eec17c7d53
William Jay Inclino <wjay.inclino@gmail.com>
2023-12-15 17:49:01 +0800

    use ip address of host machine for env

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ import api from './api'
 
 export const config = {
     api,
-    baseUrl: import.meta.env.VITE_DOMAIN + import.meta.env.BASE_URL,
+    baseUrl: import.meta.env.BASE_URL,
+    // baseUrl: import.meta.env.VITE_DOMAIN + import.meta.env.BASE_URL,
 };
