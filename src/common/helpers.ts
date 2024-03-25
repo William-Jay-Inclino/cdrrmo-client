9f42f0f9ef7231341c37f3ac179f9fac965e26b1
William Jay Inclino <wjay.inclino@gmail.com>
2024-03-25 11:05:11 +0800

    fix errors unused variables

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -5,20 +5,20 @@ import { UserTypeEnum } from "../user";
 export function isValidDate(dateString: any): boolean {
     // Parse the input date string
     const parsedDate = new Date(dateString);
-  
+
     // Check if the parsed date is a valid date and the input string is not NaN
     return !isNaN(parsedDate.getTime()) && parsedDate.toString() !== 'Invalid Date';
 }
 
-const validNetworkCodes = [
-    // Globe Telecom
-    '0817', '0905', '0906', '0915', '0916', '0917', '0925', '0926', '0927', '0935', '0936', '0937', '0938', '0994', '0996',
-    
-    // Smart Communications (includes Talk 'N Text, TNT)
-    '0813', '0900', '0907', '0908', '0909', '0910', '0911', '0912', '0913', '0914', '0918', '0919', '0920', '0921', '0928',
-    '0929', '0930', '0931', '0939', '0946', '0947', '0948', '0949', '0950', '0951', '0953', '0954', '0955', '0956', '0961',
-    '0963', '0965', '0966', '0967', '0968', '0969', '0970', '0981', '0989', '0992', '0998', '0999'
-];
+// const validNetworkCodes = [
+//     // Globe Telecom
+//     '0817', '0905', '0906', '0915', '0916', '0917', '0925', '0926', '0927', '0935', '0936', '0937', '0938', '0994', '0996',
+
+//     // Smart Communications (includes Talk 'N Text, TNT)
+//     '0813', '0900', '0907', '0908', '0909', '0910', '0911', '0912', '0913', '0914', '0918', '0919', '0920', '0921', '0928',
+//     '0929', '0930', '0931', '0939', '0946', '0947', '0948', '0949', '0950', '0951', '0953', '0954', '0955', '0956', '0961',
+//     '0963', '0965', '0966', '0967', '0968', '0969', '0970', '0981', '0989', '0992', '0998', '0999'
+// ];
 
 export function isValidPhoneNumber(phoneNumber: string) {
     console.log('helper.ts: isValidPhoneNumber()', phoneNumber);
@@ -57,16 +57,16 @@ export const getAge = (birthDate: Date): number => {
 
 
 export const isUserLGU = (type: UserTypeEnum): boolean => {
-    if(type === UserTypeEnum.LGU_Casual || type === UserTypeEnum.LGU_Job_Order || type === UserTypeEnum.LGU_Regular){
-        return true 
+    if (type === UserTypeEnum.LGU_Casual || type === UserTypeEnum.LGU_Job_Order || type === UserTypeEnum.LGU_Regular) {
+        return true
     }
     return false
 }
 
 
 export const isUserACDV = (type: UserTypeEnum): boolean => {
-    if(type === UserTypeEnum.ACDV_BART || type === UserTypeEnum.ACDV_CSO || type === UserTypeEnum.ACDV_PO || type === UserTypeEnum.ACDV_INDIVIDUAL){
-        return true 
+    if (type === UserTypeEnum.ACDV_BART || type === UserTypeEnum.ACDV_CSO || type === UserTypeEnum.ACDV_PO || type === UserTypeEnum.ACDV_INDIVIDUAL) {
+        return true
     }
     return false
 }
@@ -85,10 +85,9 @@ export function formatDate(date: Date) {
         ].join('-') +
         ' ' +
         [
-        padTo2Digits(date.getHours()),
-        padTo2Digits(date.getMinutes()),
-        padTo2Digits(date.getSeconds()),
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
         ].join(':')
     );
 }
-  
--- src/inventory/components/Search.vue
+++ src/inventory/components/Search.vue
@@ -6,74 +6,76 @@
                 <i class="fas fa-sync"></i>
             </button>
         </div>
-        <input @keyup.enter="onClickSearch()" type="text" v-model="searchVal" class="form-control" :placeholder="placeholder" @input="validateInput()">
+        <input @keyup.enter="onClickSearch()" type="text" v-model="searchVal" class="form-control"
+            :placeholder="placeholder" @input="validateInput()">
         <div class="input-group-append">
             <div class="btn-group">
                 <button @click="onClickSearch()" type="button" class="btn btn-primary">
                     Search for {{ searchRefObject[searchRef] }}
                 </button>
-                <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span class="sr-only"></span>
                 </button>
                 <div class="dropdown-menu">
                     <a @click="onChangeRef(SearchFieldEnum.Name)" class="dropdown-item" href="#">Name</a>
-                    <a @click="onChangeRef(SearchFieldEnum.SerialNumber)" class="dropdown-item" href="#">Serial Number</a>
+                    <!-- <a @click="onChangeRef(SearchFieldEnum.SerialNumber)" class="dropdown-item" href="#">Serial Number</a> -->
                 </div>
-                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import { computed, ref } from 'vue';
-    import { SearchFieldEnum, itemService, itemStore } from '..';
-    
+import { computed, ref } from 'vue';
+import { SearchFieldEnum, itemService, itemStore } from '..';
 
-    const searchRefObject = {
-        [SearchFieldEnum.Name]: 'Name',
-        [SearchFieldEnum.SerialNumber]: 'Serial Number',
-    }
 
-    const $item = itemStore()
+const searchRefObject = {
+    [SearchFieldEnum.Name]: 'Name',
+    // [SearchFieldEnum.SerialNumber]: 'Serial Number',
+}
 
-    const searchRef = ref<SearchFieldEnum>(SearchFieldEnum.Name)
-    const searchVal = ref('')
+const $item = itemStore()
 
-    const onChangeRef = (ref: SearchFieldEnum) => {
-        searchVal.value = ''
-        searchRef.value = ref
-    }
+const searchRef = ref<SearchFieldEnum>(SearchFieldEnum.Name)
+const searchVal = ref('')
 
-    const placeholder = computed( () => {
-        return `Search for ${searchRefObject[searchRef.value]}...`
+const onChangeRef = (ref: SearchFieldEnum) => {
+    searchVal.value = ''
+    searchRef.value = ref
+}
+
+const placeholder = computed(() => {
+    return `Search for ${searchRefObject[searchRef.value]}...`
+})
+
+const onClickSearch = async () => {
+    console.log('onClickSearch()')
+
+    if (searchVal.value === '') return
+
+    if (searchVal.value.trim() === '') return
+
+    const { currentPage, totalPages, totalItems, items } = await itemService.findAll({
+        page: $item.pagination.currentPage,
+        pageSize: $item.pagination.perPage,
+        searchField: searchRef.value,
+        searchValue: searchVal.value,
     })
+    $item.setPagination(currentPage, totalPages, totalItems)
+    $item.setItems(items)
+}
 
-    const onClickSearch = async() => {
-        console.log('onClickSearch()')
+const onClickRefresh = async () => {
+    console.log('onClickRefresh()')
+    const { currentPage, totalPages, totalItems, items } = await itemService.findAll({ page: 1, pageSize: $item.pagination.perPage })
+    $item.setPagination(currentPage, totalPages, totalItems)
+    $item.setItems(items)
+}
 
-        if(searchVal.value === '') return 
-
-        if(searchVal.value.trim() === '') return 
-
-        const {currentPage, totalPages, totalItems, items} = await itemService.findAll({
-            page: $item.pagination.currentPage, 
-            pageSize: $item.pagination.perPage,
-            searchField: searchRef.value,
-            searchValue: searchVal.value,
-        }) 
-        $item.setPagination(currentPage, totalPages, totalItems)
-        $item.setItems(items)
-    }
-
-    const onClickRefresh = async() => {
-        console.log('onClickRefresh()')
-        const {currentPage, totalPages, totalItems, items} = await itemService.findAll({page: 1, pageSize: $item.pagination.perPage}) 
-        $item.setPagination(currentPage, totalPages, totalItems)
-        $item.setItems(items)
-    }
-
-    const validateInput = () => {
-        searchVal.value = searchVal.value.replace(/[^A-Za-z0-9 ]/g, '');
-    }
+const validateInput = () => {
+    searchVal.value = searchVal.value.replace(/[^A-Za-z0-9 ]/g, '');
+}
 
 </script>
